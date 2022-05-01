fb8e09ce181a9fe13f684fdba18daeb57ffaea40
Odra Tezo <fatimatezo201831039@cunoc.edu.gt>
2022-04-30 22:13:25 -0600

    Update AsignadorNumTarjeta.java

--- src/Refactorizacion/NumeroTarjeta/AsignadorNumTarjeta.java
+++ src/Refactorizacion/NumeroTarjeta/AsignadorNumTarjeta.java
@@ -9,7 +9,7 @@ public class AsignadorNumTarjeta {
         valorActual=null; //Cada vez que ingrese aca se va a reiniciar el valor que tenia el valorActual.
         if(limiteDeCredito>=tipo.getLimite()){ //se evalua si es del tipo 1 y si tiene un salario suficiente para la aprovacion de la tarjeta
             valorActual=controlNumTarjetas.leerYAsingarNumeroEnArchivo(tipo);
-            System.out.println("Solicitud del tipo "+tipo.getNombre()+" aprovada con exito");
+            System.out.println("Solicitud del tipo "+tipo.getNombre()+" aprobada con exito");
         }
         return valorActual;
     }
